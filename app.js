8a6d147af5ef9fb663ac54e7fc8ee2ca7d266029
John Bergeron <jbergeronjr@gmail.com>
2019-07-21 14:25:27 -0400

    add functionality to loop through and sort for lead score

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://default:defaultpass1@leaddb-1p6na.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 //create schema
-const leadsSchema = new mongoose.Schema({
+const Schema = mongoose.Schema;
+const leadsSchema = new Schema({
     name: String,
     phone: String,
     email: String,
@@ -23,12 +24,13 @@ const leadsSchema = new mongoose.Schema({
     city: String,
     state: String,
     zip: String,
+    score: Number
 });
 
 
 //note: iterate before sending to database;
 
-let Leads = mongoose.model('Leads', leadsSchema,);
+let Leads = mongoose.model('Leads', leadsSchema);
 
 // routes 
 
@@ -36,13 +38,13 @@ app.post('/leadsuccess', (req, res) => {
   let values = Object.values(req.body);
   let lowerName = values[0].toLowerCase();
   let leadScore = 0;
-  console.log(values);
-  console.log(typeof(values));
   
   for(let i = 0; i < 1; i++) {
-    if(lowerName.includes('j')) {
+    if(lowerName.includes('z')) {
       leadScore += 3;
     };
+    // This next conditional is directly related to the total number of sales made. If carrier pigeon sales is 1/10, lead score 
+    //shouldn't be affected much. If it's 1/1MM, it should be affected greatly.   
     if(values[3] === 'carrier pigeon') {
       leadScore -= 10;
     } else if(values[3] === 'phone') {
@@ -50,22 +52,21 @@ app.post('/leadsuccess', (req, res) => {
     } else {
       leadScore;
     };
-    if((values[5] === 'ak') || (values[5] === 'hi')) {
+    if((values[5] === 'AK') || (values[5] === 'HI')) {
       leadScore -= 100;
     };
     if(values[6].startsWith('7')) {
       leadScore += 7;
     };
   };
- 
-  Leads.update( { '$addToSet': { 'leadscore': leadScore } })
+  
+// const leadScoreSchema = new Schema();
+// leadScoreSchema.add(leadsSchema).add({ score: Number });
 
-  console.log(values);
-  console.log('blank');
-  console.log(req.body);
-  console.log(typeof(leadScore));
-
-  let data = new Leads(res.body, leadScore);
+  let data = new Leads(req.body); 
+  Object.assign(data, {'score': leadScore});
+  console.log(leadScore);
+  console.log(data);
   data.save().then(item => {
     res.send('Lead saved to database');
   })
